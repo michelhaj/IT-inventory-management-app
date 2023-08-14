7bd779237af3a1729a62997e9e827687000e0434
michelhaj <36920883+michelhaj@users.noreply.github.com>
2023-08-14 03:14:09 +0300

    yes

--- barcode_scanner/barcode_scanner/settings.py
+++ barcode_scanner/barcode_scanner/settings.py
@@ -134,22 +134,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "theme/static/"
+# STATIC_URL = "theme/static/"
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATICFILES_DIRS = [ os.path.join(BASE_DIR,"static"), os.path.join(BASE_DIR , "theme/static"),]
+# STATICFILES_DIRS = [ os.path.join(BASE_DIR,"static"), os.path.join(BASE_DIR , "theme/static"),]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_URL = "/static/"
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "static/"
 
 
 # # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# STATICFILES_DIRS = os.path.join(BASE_DIR,"static"),
+STATICFILES_DIRS = os.path.join(BASE_DIR,"static"),
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
